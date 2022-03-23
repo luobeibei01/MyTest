cc6025b1706c7b4d3d1f7cab8f6b2c4cdfb921b0
luobeibei92 <luobeibei@inke.cn>
2022-03-23 15:33:05 +0800

    增加case

---
+++ case/test_list.py
@@ -0,0 +1,8 @@
+import pytest
+import requests
+from conf.sysconf import host
+
+def test_BoxList():
+    url = host + '/api/v1/mall/item/box/list'
+    res= requests.get(url=url,verify=False)
+    print(res.json())
---
+++ conf/sysconf.py
@@ -0,0 +1,5 @@
+import records
+
+
+host='https://testservice.muju001.com'
+# UC_DB = records.Database('mysql://inke_db_testuser:inekoedb0testgv3@tx5-inno-funbox-test-db01.bj:3306/user_account')
