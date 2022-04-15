d3d64eefbb6c210314bdaf7bb22ab15552bf0c0e
luobeibei <luobeibei@inke.cn>
2022-04-15 11:52:57 +0800

    1
    1

--- conf/sysconf.py
+++ conf/sysconf.py
@@ -2,7 +2,7 @@ import records
 
 
 host='https://testservice.muju001.com'
-# UC_DB = records.Database('mysql://inke_db_testuser:inekoedb0testgv3@tx5-inno-funbox-test-db01.bj:3306/user_account')
+UC_DB = records.Database('mysql://inke_db_testuser:inekoedb0testgv3@tx5-inno-funbox-test-db01.bj:3306/user_account')
 # list=[{1},{2},{3}]
 # print(list[0])
 # for i in list:
