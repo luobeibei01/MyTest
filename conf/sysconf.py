a9380f8d14c0c7eb29f7648c26bbcec0f9f50aff
luobeibei <luobeibei@inke.cn>
2022-04-20 14:01:44 +0800

    1
    1

--- conf/sysconf.py
+++ conf/sysconf.py
@@ -3,7 +3,7 @@ import pymysql
 pymysql.install_as_MySQLdb()
 
 host='https://testservice.muju001.com'
-db = records.Database('mysql://inke_db_testuser:inekoedb0testgv3@tx5-inno-funbox-test-db01.bj:3306/funbox_user_account')
+account = records.Database('mysql://inke_db_testuser:inekoedb0testgv3@tx5-inno-funbox-test-db01.bj:3306/funbox_user_account')
 # id=db.query('select id from funbox_user_account.user_account where phone=11111112000')
 
 # list=[{1},{2},{3}]
